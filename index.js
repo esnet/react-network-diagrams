19f7a4366e2d076c5233c8225c6733056d0f358e
Peter Murphy <peter71@gmail.com>
2015-06-18 11:25:13 -0700

    Fix the entry to point correctly to the built files

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-"use strict";
-
-//
-// Entry point
-//
-
 module.exports = {
-    Map: require("./build/lib/components/map.jsx"),
-    TrafficMap: require("./build/lib/components/traffic-map.jsx"),
-    Legend: require("./build/lib/components/map.jsx")
+    Map: require("./build/lib/components/map"),
+    TrafficMap: require("./build/lib/components/traffic-map"),
+    Legend: require("./build/lib/components/map")
 }

36e9a25a711ea64ac73c33d22df45529052113b5
Peter Murphy <peter71@gmail.com>
2015-07-09 05:16:21 -0700

    Fix typo in map exports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
     Map: require("./build/npm/lib/map.js"),
     TrafficMap: require("./build/npm/lib/traffic-map.js"),
-    Legend: require("./build/npm/lib/map.js")
+    Legend: require("./build/npm/lib/legend.js")
 }

cfbfde3c6450e5e22f5c89e62c7db2707e782731
Peter Murphy <pmurphy@es.net>
2016-05-18 16:58:35 -0700

    Move index.js file into src

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ *  Copyright (c) 2015, The Regents of the University of California,
+ *  through Lawrence Berkeley National Laboratory (subject to receipt
+ *  of any required approvals from the U.S. Dept. of Energy).
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+export BaseMap from "./map-base.js";
+export TrafficMap from "./traffic-map.js";
+export MapLegend from "./map-legend.js";
+export Connection from "./circuit-diagram-connection";
+export Endpoint from "./circuit-diagram-endpoint";
+export BasicCircuit from "./circuit-diagram-basic.js";
+export ConcatenatedCircuit from "./circuit-diagram-concatenated.js";
+export ParallelCircuit from "./circuit-diagram-parallel.js";
+export PatchPanel from "./circuit-diagram-patchpanel.js";
+export Resizable from "./resizable.js";
+export MapEditor from "./map-editor.js";
